d9d0813121e3aaa39e75282630c12b029663450f
Rupert Rutledge <1982481+Eosis@users.noreply.github.com>
2022-02-16 23:29:39 +0000

    Try out the port code in a more functional style

--- src/port.rs
+++ src/port.rs
@@ -1,6 +1,8 @@
 use std::time::Duration;
 
-use serialport::{Error, ErrorKind, Result, SerialPort, SerialPortType};
+use serialport::{Error, ErrorKind, Result, SerialPort, SerialPortInfo, SerialPortType};
+use itertools::Itertools;
+use itertools::FoldWhile::{Continue, Done};
 
 use crate::common::MESSAGE_LENGTH;
 
@@ -11,39 +13,38 @@ const PRODUCT: &str = "CH340";
 const BAUD_RATE: u32 = 9600;
 const TIMEOUT_S: u64 = 1;
 
-pub fn connect() -> Result<Box<dyn SerialPort>> {
-    let mut result = Error::new(ErrorKind::NoDevice, "Cannot find power supply");
-
-    let ports_visible = serialport::available_ports()?;
-
-    for port_visible in ports_visible {
-        match &port_visible.port_type {
-            SerialPortType::UsbPort(usb_port_info) => {
-                match (&usb_port_info.manufacturer, &usb_port_info.product) {
-                    (Some(manufacturer), Some(product)) => {
-                        if !manufacturer.eq(MANUFACTURER_0) && !manufacturer.eq(MANUFACTURER_1) {
-                            continue;
-                        }
-                        if !product.contains(PRODUCT) {
-                            continue;
-                        }
-                    }
-                    _ => continue,
-                }
-            }
-            _ => continue,
-        }
-
-        match serialport::new(&port_visible.port_name, BAUD_RATE)
-            .timeout(Duration::from_secs(TIMEOUT_S))
-            .open()
-        {
-            Ok(port) => return Ok(port),
-            Err(err) => result = err,
-        }
+fn match_manufacturer_and_product(port_info: &SerialPortInfo) -> bool {
+    if let SerialPortType::UsbPort(usb_port_info) = &port_info.port_type {
+        let manufacturer_matched = usb_port_info.manufacturer.as_ref()
+            .map(|man| man == MANUFACTURER_0 || man == MANUFACTURER_1)
+            .unwrap_or(false);
+        let product_matched = usb_port_info.product.as_ref()
+            .map(|product| product.contains(PRODUCT))
+            .unwrap_or(false);
+        manufacturer_matched && product_matched
+    } else {
+        false
     }
+}
 
-    Err(result)
+pub fn connect() -> Result<Box<dyn SerialPort>> {
+    let default_err = Err(Error::new(ErrorKind::NoDevice, "Cannot find power supply"));
+    serialport::available_ports()?
+        .into_iter()
+        .filter(match_manufacturer_and_product)
+        .map(|port_info| {
+            serialport::new(&port_info.port_name, BAUD_RATE)
+                .timeout(Duration::from_secs(TIMEOUT_S))
+                .open()
+        })
+        .fold_while(default_err, |_, new| {
+            if new.is_ok() {
+                Done(new)
+            } else {
+                Continue(new)
+            }
+        })
+        .into_inner()
 }
 
 pub fn read(port: &mut Box<dyn SerialPort>) -> Result<[u8; MESSAGE_LENGTH]> {
